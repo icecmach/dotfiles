6f6048432292c30ba9ca46b3cce03473de4d2b3a
icecmach <app.connected404@passinbox.com>
2025-08-30 14:31:20 -0400

    Replace user-dbus-check by dbus-dinit-user

--- dinit/.config/dinit.d/scripts/user-dbus-check.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-while ! pgrep -G $(id -g "$USER") -x "dbus-daemon" >/dev/null; do
-echo "[$(date)] Waiting for user dbus daemon"
-sleep 0.1
-done
-echo "[$(date)] User dbus daemon running"
-exit 0
